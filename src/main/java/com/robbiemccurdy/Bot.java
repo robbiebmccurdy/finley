75b8317ebf3a7b4c41e05908dcff3dc395679a0b
Robbie McCurdy <robbiebmccurdy@gmail.com>
2023-03-30 15:47:20 -0500

    First Commit!

---
+++ src/main/java/com/robbiemccurdy/Bot.java
@@ -0,0 +1,40 @@
+package com.robbiemccurdy;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
+public class Bot {
+
+    private final Dotenv config;
+    private final ShardManager shardManager;
+
+    public Bot() throws LoginException {
+        config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(token);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.playing("ur mom"));
+        shardManager = builder.build();
+    }
+
+    public Dotenv getConfig() {
+        return config;
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Bot bot = new Bot();
+        } catch (LoginException e) {
+            System.out.println("ERROR: Bot Token is INVALID");
+        }
+    }
+}
